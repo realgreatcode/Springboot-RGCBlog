6f3f37c3ab010b071eb9af13295e173fab8071a1
BE_YangJinho <real.great.code@gmail.com>
2023-04-19 21:17:32 +0900

    initial commit

---
+++ src/main/java/shop/realgreatcode/rgcblog/RgcblogApplication.java
@@ -0,0 +1,13 @@
+package shop.realgreatcode.rgcblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgcblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgcblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/realgreatcode/rgcblog/RgcblogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.realgreatcode.rgcblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgcblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
