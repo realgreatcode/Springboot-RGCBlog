f7ca4f5a1e37aad0d48aab35f3382c641eb752f8
BE_YangJinho <real.great.code@gmail.com>
2023-04-21 20:08:54 +0900

    1단계 회원가입 뷰 완료

--- src/main/java/shop/realgreatcode/rgcblog/RgcblogApplication.java
+++ src/main/java/shop/realgreatcode/rgcblog/RgcblogApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.realgreatcode.rgcblog.model.user.User;
 import shop.realgreatcode.rgcblog.model.user.UserRepository;
@@ -11,6 +12,7 @@ import shop.realgreatcode.rgcblog.model.user.UserRepository;
 @SpringBootApplication
 public class RgcblogApplication {
 
+	@Profile("dev")
 	@Bean
 	CommandLineRunner init(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
 		return args -> {
